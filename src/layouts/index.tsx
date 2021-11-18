5b5d8eec9aeccaa6fdc603c1effd3e4259699751
nextdooroldwang <278188438@qq.com>
2021-11-18 15:57:28 +0900

    mfsu build

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,8 +2,8 @@ import { IRouteComponentProps } from 'umi';
 
 export default function Layout({ children, location }: IRouteComponentProps) {
   if (location.pathname === '/login') {
-    return <div>Login----{children}</div>;
+    return <div>Login---{children}</div>;
   }
 
-  return <div>Layout----{children}</div>;
+  return <div>Layout--{children}</div>;
 }
